bdc554776c3d762349db1be877f8a6d9697a6bde
tornvall <f.tornvall@gmail.com>
2013-01-23 00:37:19 +0100

    Input and camera objects
    At last the time has come for the InputHandler and Camera to make their
    first appearances

---
+++ XNAShadingEffects/XNAShadingEffects/Input/ActionType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XNAShadingEffects.Input {
+    public enum ActionType {
+        IncreaseSpeed,
+        DecreaseSpeed,
+        Left,
+        Right,
+        Up,
+        Down,
+        RollLeft,
+        RollRight,
+        PitchUp,
+        PitchDown,
+        YawLeft,
+        YawRight,
+        Quit,
+        IncreaseSealevel,
+        DecreaseSealevel,
+        PlayerUp,
+        PlayerDown,
+        PlayerLeft,
+        PlayerRight,
+        SwitchMode
+    }
+}
---
+++ XNAShadingEffects/XNAShadingEffects/Input/UserAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XNAShadingEffects.Input {
+    public class UserAction {
+        public delegate void ActionEvent(ActionType Type);
+
+        public ActionType Type;
+
+        public event ActionEvent ActionPerformed;
+
+        public UserAction(ActionType type) {
+            Type = type;
+        }
+
+        public void FireEvent() {
+            ActionPerformed(Type);
+        }
+    }
+}
